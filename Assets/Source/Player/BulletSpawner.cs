6b353a04dede533f998a3d9a124fb2e42f3b970f
Александр Гяч <as.gyach@gmail.com>
2024-09-16 20:08:07 +0300

    tweak: bullet changing

--- Assets/Source/Player/BulletSpawner.cs
+++ Assets/Source/Player/BulletSpawner.cs
@@ -4,5 +4,5 @@ public class BulletSpawner
 {
     public void Spawn(Vector3 position, Bullet bullet, Vector3 mousePosition)
 		=> GameObject.Instantiate(bullet, position, Quaternion.identity)
-		.SetDirection((mousePosition - Camera.main.WorldToScreenPoint(position)).normalized);
+    	.SetDirection((mousePosition - Camera.main.WorldToScreenPoint(position)).normalized);
 }
